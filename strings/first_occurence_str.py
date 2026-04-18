9e472cce0d8d4b87cad5cdc5b1756ca94efaafeb
Python codes <snehamittal.sm22@gmail.com>
2026-04-18 15:07:32 +0530

    commit

--- strings/first_occurence_str.py
+++ strings/first_occurence_str.py
@@ -7,14 +7,12 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         n, m = len(haystack), len(needle)
-        
         for i in range(n - m + 1):
             if haystack[i : i + m] == needle:
                 return i
-        
         return -1
-
-
+    
+   
 # ---- Test Cases ----
 solution = Solution()
 
